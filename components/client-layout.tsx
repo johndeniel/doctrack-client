d8a5f2cb6eeda67e3e26e58f3977f2efae3dc546
johndeniel <johndeniel.d37@gmail.com>
2025-03-27 08:39:59 +0800

    fix the scroll bar

--- components/client-layout.tsx
+++ components/client-layout.tsx
@@ -2,24 +2,23 @@
 
 import { Toaster } from "@/components/ui/sonner";
 import { useRouter } from "next/navigation"
-import { cn } from "@/lib/utils";
 import { FloatingDock } from "@/components/ui/floating-dock"
-import { IconUsers, IconUserPlus } from "@tabler/icons-react"
+import { IconHome, IconCalendar } from "@tabler/icons-react"
 import { ThemeProvider } from "@/components/theme-provider"
 
 const links = [
   {
-    title: "All Accounts",
-    icon: <IconUsers className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+    title: "Home",
+    icon: <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
     href: "/",
-    view: "all-accounts",
+    view: "home",
   },
   {
-    title: "Create Account",
-    icon: <IconUserPlus className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-    href: "/create-user-account",
-    view: "create-account",
-  },
+    title: "Calendar",
+    icon: <IconCalendar className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+    href: "/calendar",
+    view: "calendar",
+  }
 ]
 
 export function ClientLayout({
@@ -30,50 +29,44 @@ export function ClientLayout({
   const router = useRouter()
 
   return (
-    <>
-      <main className="relative min-h-screen w-full bg-white dark:bg-black">
-        {/* Dot pattern background */}
-        <div
-          className={cn(
-            "absolute inset-0",
-            "[background-size:20px_20px]",
-            "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
-            "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
-          )}
-          aria-hidden="true"
-        />
-
-        {/* Radial gradient overlay */}
-        <div
-          className="pointer-events-none absolute inset-0 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"
-          aria-hidden="true"
-        />
-
-        {/* Content container */}
-        <div className="relative z-10 mx-auto h-full max-w-7xl p-4 md:p-8">
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <div className="flex flex-col h-screen w-screen overflow-hidden">
+      <main className="flex-grow overflow-auto bg-white dark:bg-black">
+        <div className="h-full w-full max-w-7xl mx-auto px-4 md:px-8">
+          <ThemeProvider 
+            attribute="class" 
+            defaultTheme="system" 
+            enableSystem 
+            disableTransitionOnChange
+          >
             {children}
           </ThemeProvider>
-
-          {/* Navigation dock */}
-          <FloatingDock
-            items={links.map((link) => ({
-              ...link,
-              onClick: () => router.push(link.href),
-            }))}
-            desktopClassName="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50"
-            mobileClassName="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50"
-          />
         </div>
       </main>
+
+      {/* Navigation dock */}
+      <FloatingDock
+        items={links.map((link) => ({
+          ...link,
+          onClick: () => router.push(link.href),
+        }))}
+        desktopClassName="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50"
+        mobileClassName="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50"
+      />
+      
       <Toaster
         toastOptions={{
           classNames: {
-            toast:
-              'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg group-[.toaster]:pointer-events-auto',
+            toast: `
+              group toast 
+              group-[.toaster]:bg-background 
+              group-[.toaster]:text-foreground 
+              group-[.toaster]:border-border 
+              group-[.toaster]:shadow-lg 
+              group-[.toaster]:pointer-events-auto
+            `,
           },
         }}
       />
-    </>
+    </div>
   );
 }
